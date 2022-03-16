da9859a546d00567919b98418d760651507e0428
Warbacon <joaquinguerratocino@gmail.com>
2022-03-17 00:29:30 +0100

    Added onedark.fish

---
+++ onedark.fish
@@ -0,0 +1,33 @@
+# One Dark Color Palette
+set -l foreground abb2bf
+set -l selection 343a46
+set -l comment 7f848e
+set -l red e06c75
+set -l orange d19a66
+set -l yellow e5c07b
+set -l green 98c379
+set -l purple c678dd
+set -l cyan 56b6c2
+set -l pink ff79c6
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
