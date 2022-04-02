cd33b27be96eed24b01179e8f69ec75ae5279bfd
Warbacon <joaquinguerratocino@gmail.com>
2022-04-02 14:53:25 +0200

    Updated fish config

--- .config/fish/arch_install.fish
+++ .config/fish/arch_install.fish
@@ -1,4 +1,6 @@
 #!/bin/fish
 
-sudo pacman -S lsd fisher
+sudo pacman -S exa fisher fzf bat fd
 fisher install jorgebucaran/autopair.fish
+fisher install PatrickF1/fzf.fish
+fisher install IlanCosman/tide@v5
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,3 @@
 if status is-interactive
     set fish_greeting
-    
-    # export MANPAGER="nvim  +Man!"
-
-    abbr --add --global q exit
-    abbr --add --global clr clear
-    alias ls=lsd
-    
-    starship init fish | source
 end
